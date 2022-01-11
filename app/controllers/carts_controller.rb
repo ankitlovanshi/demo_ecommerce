46a4ca6504bf8b38f9303d0a0b21a2bc8bd98ffc
Ankit <ankitlovanshi622@gmail.com>
2022-01-11 18:31:12 +0530

    Remove to cart button add

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,10 @@ class CartsController < ApplicationController
   def index
     @cart_items = current_user.cart.cart_items
   end
+
+  def destroy
+    @cart_items = CartItem.find(params[:id])
+    @cart_items.destroy
+    redirect_to root_path
+  end
 end
